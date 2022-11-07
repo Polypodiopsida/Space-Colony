c498eeab9b6d86947be67d89ca2bc6aa6760547f
Fern <115055626+Polypodiopsida@users.noreply.github.com>
2022-11-07 12:13:49 -0600

    Updated camera system
    Camera moves on WASD keys, zooms in and out with the scroll wheel, and resets FOV when middle mouse is pressed

--- CameraControl.gd
+++ CameraControl.gd
@@ -1,22 +1,39 @@
 extends Camera
 
-var mouseSensitivity = 0.05
-var currentFOV
+var mouseSensitivity = 40
+var currentFOV = 90
 
-var xRot
-var yRot
+var ud = 0
+var lr = 0
 
 func _process(delta):
+	if current:
+		if Input.is_action_pressed("cam_up") && !Input.is_action_pressed("cam_down"):
+			ud = 1
+		elif Input.is_action_pressed("cam_down") && !Input.is_action_pressed("cam_up"):
+			ud = -1
+		else:
+			ud = 0
+		if Input.is_action_pressed("cam_left") && !Input.is_action_pressed("cam_right"):
+			lr = -1
+		elif Input.is_action_pressed("cam_right") && !Input.is_action_pressed("cam_left"):
+			lr = 1
+		else:
+			lr = 0
+			
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				currentFOV -= 1
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				currentFOV += 1
+			if event.button_index == BUTTON_MIDDLE:
+				currentFOV = 90
 	
-	var ud = 0
-	var lr = 0
+func _physics_process(delta):
+	rotation_degrees.x += ud * mouseSensitivity * delta
+	rotation_degrees.y -= lr * mouseSensitivity * delta
+	currentFOV = clamp(currentFOV, 70, 110)
 	
-	if Input.is_action_pressed("cam_up") && !Input.is_action_pressed("cam_down"):
-		ud = 1
-	if Input.is_action_pressed("cam_down") && !Input.is_action_pressed("cam_up"):
-		ud = -1
-	if Input.is_action_pressed("cam_left") && !Input.is_action_pressed("cam_right"):
-		lr = -1
-	if Input.is_action_pressed("cam_right") && !Input.is_action_pressed("cam_left"):
-		lr = 1
-	
+	fov = currentFOV
